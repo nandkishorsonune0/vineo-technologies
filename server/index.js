05db8a6d5b5529f4243ef7b30e273fe25aca8aca
Nandkishor Sonune <102716176+nandkishorsonune0@users.noreply.github.com>
2024-11-25 17:34:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const Express=require("express");
 const fs = require("fs");
-const path = require("path");
 const PORT = 5000;
 const cors=require("cors")
 
@@ -10,17 +9,31 @@ const cors=require("cors")
 const server= Express();
 server.use(cors());
 
-server.get("/data",(req, res)=>{
-    fs.readFile("./data.json", (err, data)=>{
+server.get("/data",(req, res, next )=>{
+    fs.readFile("./data.json", (err, data, )=>{
         if(err){
             console.log("Json File Error", err);
-            res.status(500).send("Internal server error");
-            return
-        }
+            // res.status(500).send("Internal server error");
+            next(err)
+            // return
+
+        }else 
         res.json(JSON.parse(data));
     });
 });
-
+server.use("/",(req, res, next)=>{
+    console.log("Hello");
+    next({
+        statusCode: 400,
+        name: 'USER_ERROR_NAME',
+        message: 'Bad Request. No such endpoint',
+    });
+    
+})
+server.use( ( err, req, res, next ) => {
+    console.log(err)
+    res.status(403).send(err.message);
+} )
 server.listen(PORT, ()=>{
     console.log("Server is running on port", PORT);
 })
